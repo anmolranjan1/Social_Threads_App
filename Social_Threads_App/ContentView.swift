49e5152877c03fbdcf8e52a5649efc2abf1b3a0a
Anmol Ranjan <anmolranjan1@gmail.com>
2024-07-07 10:45:51 +0530

    Initial Commit

---
+++ Social_Threads_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Social_Threads_App
+//
+//  Created by Anmol Ranjan on 07/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Social_Threads_App/Social_Threads_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_Threads_AppApp.swift
+//  Social_Threads_App
+//
+//  Created by Anmol Ranjan on 07/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Social_Threads_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
