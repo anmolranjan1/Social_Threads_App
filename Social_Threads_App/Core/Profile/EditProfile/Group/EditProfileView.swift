79c583325c1c5fcad6100eaea11b0fe4da797f13
Anmol Ranjan <anmolranjan1@gmail.com>
2024-07-08 22:42:39 +0530

    Added Profile Page and Create Thread Page

---
+++ Social_Threads_App/Core/Profile/EditProfile/Group/EditProfileView.swift
@@ -0,0 +1,96 @@
+//
+//  EditProfileView.swift
+//  Social_Threads_App
+//
+//  Created by Anmol Ranjan on 08/07/24.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    @State private var bio = ""
+    @State private var link = ""
+    @State private var isPrivateProfile = false
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color(.systemGroupedBackground)
+                    .edgesIgnoringSafeArea([.bottom, .horizontal])
+                
+                VStack {
+                    // name and profile image
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Name")
+                                .fontWeight(.semibold)
+                            
+                            Text("Anmol Ranjan")
+                        }
+                        .font(.footnote)
+                        
+                        Spacer()
+                        
+                        CircularProfileImageView()
+                    }
+                    
+                    Divider()
+                    
+                    // bio field
+                    VStack(alignment: .leading) {
+                        Text("Bio")
+                            .fontWeight(.semibold)
+                        
+                        TextField("Enter your bio..", text: $bio, axis: .vertical)
+                    }
+                    
+                    Divider()
+                    
+                    VStack(alignment: .leading) {
+                        Text("Link")
+                            .fontWeight(.semibold)
+                        
+                        TextField("Add link...", text: $link)
+                    }
+                    
+                    Divider()
+                    
+                    Toggle("Private profile", isOn: $isPrivateProfile)
+                }
+                .font(.footnote)
+                .padding()
+                .background(.white)
+                .cornerRadius(10)
+                .padding()
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color(.systemGray4), lineWidth: 1)
+                }
+            }
+            .navigationTitle("Edit Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.black)
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Done") {
+                        
+                    }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
