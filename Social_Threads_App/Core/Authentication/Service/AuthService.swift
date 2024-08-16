0f884d43571fd1ef80be07894bec1f2232bc1565
Anmol Ranjan <anmolranjan1@gmail.com>
2024-08-16 18:01:29 +0530

    Profile Updates Implemented

--- Social_Threads_App/Core/Authentication/Service/AuthService.swift
+++ Social_Threads_App/Core/Authentication/Service/AuthService.swift
@@ -23,6 +23,7 @@ class AuthService {
         do {
             let result = try await Auth.auth().signIn(withEmail: email, password: password)
             self.userSession = result.user
+            try await UserService.shared.fetchCurrentUser()
         } catch {
             print("Failed to create user with \(error.localizedDescription)")
         }
@@ -42,6 +43,7 @@ class AuthService {
     func signOut() {
         try? Auth.auth().signOut() // signOut from backend
         self.userSession = nil // removes session locally and updates routing
+        UserService.shared.reset() // sets current user object to nil
     }
     
     @MainActor
@@ -54,5 +56,6 @@ class AuthService {
         let user = User(id: id, fullname: fullname, email: email, username: username)
         guard let userData = try? Firestore.Encoder().encode(user) else { return }
         try await Firestore.firestore().collection("users").document(id).setData(userData)
+        UserService.shared.currentUser = user
     }
 }
--- Social_Threads_App/Services/UserService.swift
+++ Social_Threads_App/Services/UserService.swift
@@ -25,4 +25,8 @@ class UserService {
         
         print("Debug: User is \(user)")
     }
+    
+    func reset() {
+        self.currentUser = nil
+    }
 }
