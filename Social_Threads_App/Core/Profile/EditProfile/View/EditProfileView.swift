c04fc3c0a8e3a68a555c76b1e53db11860f97c65
Anmol Ranjan <anmolranjan1@gmail.com>
2024-08-17 23:54:17 +0530

    User Images Loading Added

--- Social_Threads_App/Core/Profile/EditProfile/View/EditProfileView.swift
+++ Social_Threads_App/Core/Profile/EditProfile/View/EditProfileView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import PhotosUI
 
 struct EditProfileView: View {
+    let user: User
     @State private var bio = ""
     @State private var link = ""
     @State private var isPrivateProfile = false
@@ -28,7 +29,7 @@ struct EditProfileView: View {
                             Text("Name")
                                 .fontWeight(.semibold)
                             
-                            Text("Anmol Ranjan")
+                            Text(user.fullname)
                         }
                         .font(.footnote)
                         
@@ -42,7 +43,7 @@ struct EditProfileView: View {
                                     .frame(width: 40, height: 40)
                                     .clipShape(Circle())
                             } else {
-                                CircularProfileImageView()
+                                CircularProfileImageView(user: user, size: .small)
                             }
                         }
                     }
@@ -105,6 +106,8 @@ struct EditProfileView: View {
     }
 }
 
-#Preview {
-    EditProfileView()
+struct EditProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileView(user: dev.user)
+    }
 }
